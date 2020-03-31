a5b94e0cf83d96c7fd18dd45460540222445a97c
rishikM99 <61158240+rishikM99@users.noreply.github.com>
2020-03-31 22:05:41 +0530

    Update Node.java

--- src/problem5/node/Node.java
+++ src/problem5/node/Node.java
@@ -4,7 +4,32 @@
  *  Date: 23-Mar-20
  *  Time: 7:06 PM
  */
+import problem5.student.*;
 package problem5.node;
 // to define node properties
 public class Node {
+  public String toString() {
+		return "["+data + ", "+ next + "]";
+	}
+	private Student data;
+	private Node next;
+	public Node(Student data) {
+		this.data=data;
+		next=null;
+		// TODO Auto-generated constructor stub
+	}
+	public Node getNext() {
+		return next;
+	}
+	public void setNext(Node next) {
+		this.next = next;
+	}
+	public Student getData() {
+		return data;
+	}
+	public void setData(int data) {
+		this.data = data;
+	}
+
+
 }
