3ff586fcd20106c235a9c6b6d85b5e54c433e63f
rishikM99 <61158240+rishikM99@users.noreply.github.com>
2020-03-31 20:24:41 +0530

    Update Node.java
    node class updated for student class type data

--- src/problem3/node/Node.java
+++ src/problem3/node/Node.java
@@ -4,7 +4,29 @@
  *  Date: 23-Mar-20
  *  Time: 7:13 PM
  */
+import problem3.student.Student;
 package problem3.node;
 
 public class Node {
+	private Student data=new Student();
+	private Node next;
+	public Node(Student data) {
+		this.data=data;
+		next=null;
+		// TODO Auto-generated constructor stub
+	}
+	public Node getNext() {
+		return next;
+	}
+	public void setNext(Node next) {
+		this.next = next;
+	}
+	public Student getData() {
+		return data;
+	}
+	public void setData(int data) {
+		this.data = data;
+	}
+
+
 }
