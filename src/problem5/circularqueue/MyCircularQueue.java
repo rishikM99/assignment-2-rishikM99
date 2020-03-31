cfb547b6e9cb9f358037e99ea58c53144889bae0
rishikM99 <61158240+rishikM99@users.noreply.github.com>
2020-03-31 22:07:43 +0530

    Update MyCircularQueue.java

--- src/problem5/circularqueue/MyCircularQueue.java
+++ src/problem5/circularqueue/MyCircularQueue.java
@@ -7,4 +7,70 @@
 package problem5.circularqueue;
 //to implement circular queue
 public class MyCircularQueue {
+  private Student[] queue;
+	private int head;
+	private int tail;
+	private int population;
+	
+
+	@Override
+	public String toString() {
+		return "CircularQueue [queue=" + Arrays.toString(queue) + ", head=" + head + ", tail=" + tail + ", population="
+				+ population + "]";
+	}
+
+	public CircularQueue(int size) {
+		queue=new Student[size];
+		head=0;
+		tail=0;
+		population=0;
+		
+	}
+	
+	public boolean isEmpty() {
+		boolean response=false;
+		if(population==0) {
+			response=true;
+		}
+		return(response);
+	}
+	
+	
+	public boolean enqueue(Student data) {
+		
+			if(population<queue.length) {
+				queue[tail]=data;
+				tail=(tail+1)%(queue.length);
+				population=population+1;
+				return(true);
+			}
+			else {
+				return(false);
+			}
+	}
+	public Student dequeue() {
+		if(isEmpty()==false) {
+			head=(head+1)%(queue.length);
+			population=population-1;
+			return(queue[head-1]);
+		}
+		else {
+			return(-1);
+		}
+	}
+	public Student peek() {
+		if(isEmpty()==false && (population)>=2) {
+			return(queue[head+1]);
+		}
+		else {
+			return(-1);
+		}
+			
+	}
+
+	public int getPopulation() {
+		return population;
+	}
+
+
 }
