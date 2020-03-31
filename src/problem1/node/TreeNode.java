cf176e5a9b2b21d4d31bc4b57cc00dc6294db4a6
rishikM99 <61158240+rishikM99@users.noreply.github.com>
2020-03-31 15:05:57 +0530

    Update TreeNode.java
    updated the node class for problem 1

--- src/problem1/node/TreeNode.java
+++ src/problem1/node/TreeNode.java
@@ -7,4 +7,39 @@
 package problem1.node;
 
 public class TreeNode {
+  private int data;
+	private TreeNode lower;
+	private TreeNode higher;
+	
+	public TreeNode(int data) {
+		this.data=data;
+		higher=null;
+		lower=null;
+	}
+
+	public int getData() {
+		return data;
+	}
+
+	public void setData(int data) {
+		this.data = data;
+	}
+
+	public TreeNode getLower() {
+		return lower;
+	}
+
+	public void setLower(TreeNode lower) {
+		this.lower = lower;
+	}
+
+	public TreeNode getHigher() {
+		return higher;
+	}
+
+	public void setHigher(TreeNode higher) {
+		this.higher = higher;
+	}
+	
+
 }
