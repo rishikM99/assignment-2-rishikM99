f4bd9d3c85f605ccf5605ab174f867f464b6be13
rishikM99 <61158240+rishikM99@users.noreply.github.com>
2020-03-31 22:03:30 +0530

    Update Student.java

--- src/problem5/student/Student.java
+++ src/problem5/student/Student.java
@@ -7,4 +7,25 @@
 package problem5.student;
 // to store student information and properties
 public class Student {
+  private int id;
+  private int backlogs;
+  private int appearCount;
+  public void setId(int id){
+    this.id=id;
+  }
+  public int getId(){
+    return(this.id);
+  }
+  public void setbacklogs(int bg){
+    this.backlogs=bg;
+  }
+  public int getbacklogs(){
+    return(this.backlogs);
+  }
+  public void setappearCount(int ac){
+    this.appearCount=id;
+  }
+  public int getappearCount(){
+    return(this.appearCount);
+  }
 }
